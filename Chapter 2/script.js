bd46fd0c0fea4c6fc089e5ebe42ef96720203865
chitresh99 <chitreshpoojary990@gmail.com>
2024-04-04 13:41:18 +0530

    finished learning variables

---
+++ Chapter 2/script.js
@@ -0,0 +1,9 @@
+let name = "chitresh"; //we can use single quotes or double quotes but single quotes is a better practice
+console.log(name);
+
+//now i know that i used double quotes because i have that c++ habit
+//also your variable names should be meaningful
+//also your variable name cannot be a javascript keyword 
+//it cannot start with a number (1name)
+//cannot contain a space or a python
+//best practice is to declare each variable on a single line 
