e381beee4429ea31ec1e16fe71180d13639be77a
chitresh99 <chitreshpoojary990@gmail.com>
2024-04-05 11:59:23 +0530

    finished objects

---
+++ Chapter 4/script.js
@@ -0,0 +1,14 @@
+//Reference type - Objects,Arrays,Functions
+//GROUPING ALL THE THINGS TOGHETHER IS WHAT ALL THE OBJECT DOES
+let song = {
+    title:"Woh raat",
+    genre:"Hip-hop",
+    rating:10
+};
+
+console.log(song)
+console.log(typeof(song));
+
+//accessing particular things
+console.log(song.title); //first way
+console.log(song['title']); //second way
