8b58673b3fd4121dd2d63f2b1e36caed1a33bf3c
chitresh99 <chitreshpoojary990@gmail.com>
2024-04-05 13:11:53 +0530

    finished learning arrays

---
+++ Chapter 6/script.js
@@ -0,0 +1,15 @@
+//arrays in javascript
+
+//in arrays we put in a list and we access them using numeric indexes
+// we can also add boolean and null
+let names = ["chitresh","siddesh","raj","varun",6,true,null];
+
+console.log(names[3]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[5]);
+
+console.log(typeof(names));//this will give the output objects
+
+//arrays are objects
+//the only reference type is actually object
