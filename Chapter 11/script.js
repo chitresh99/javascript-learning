f84e695e84c03d7859273304d4f6588388fcd1b9
chitresh99 <chitreshpoojary990@gmail.com>
2024-04-16 21:38:11 +0530

    implemented higher order function part 2

--- Chapter 11/script.js
+++ Chapter 11/script.js
@@ -1,7 +1,7 @@
 //functions in javascript are also called first class citizens
 
 //first way to write functions
-function add(a,b){
+function add(a,b){//memory is allocated for the function here
  return a+b;
 }
 console.log(add);
@@ -9,13 +9,15 @@ console.log(add(2,3));
 
 //second way to write functions
 
-let sum = function(a,b){
+let sum = function(a,b){//memory is allocated for the variable here
     return a+b;
-}
+}//use that refresh icon near the scope to check the local
 
 console.log(sum);
 console.log(sum(2,3));
 
+
+//third way of function this will be declared in global
 var sum2 = function(a,b){
     return a+b;
 }
