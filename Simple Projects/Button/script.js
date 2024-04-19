77222387ae54aebe5f49a19ab5dbeb12567a8296
chitresh99 <chitreshpoojary990@gmail.com>
2024-04-19 13:36:07 +0530

    implemented a button click

---
+++ Simple Projects/Button/script.js
@@ -0,0 +1,12 @@
+document.getElementById('mybutton').addEventListener('click',function(){
+console.log('button clicked');
+});
+
+//explanation of this code :-
+// document :- here document represents the entire html document
+// getElementById('myButton'): This method is used to select an HTML element based on its id attribute. 
+// In this case, it selects the element with the id of "myButton"
+
+// addeventlistner :- this attaches and event to that element like in our case we have a click 
+// function :- in this function block , we write the function for that element, when the button is clicked whatever is written here will be performed
+
